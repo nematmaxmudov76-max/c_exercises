314a124ec417fea98bd53c1d0d4b7fb4cf564d76
Nematjon <nematmaxmudov76@.com>
2025-09-20 17:41:31 +0500

    birinchi commit

---
+++ project.py
@@ -0,0 +1,145 @@
+#1
+print("Hello world")
+ 
+ #2
+a=input("Ismingizni kiriting:")
+print(f"Salom:{a}")
+
+ #3
+a=input("Ismingizni kiriting:")
+b=input("Familyangizni kiriting:")
+print(f"Ism:{a}, Familya:{b}")
+  #4
+a,b,c=1,2,3
+print(a,b,c, set=",")
+
+#5
+print("Phyton, is", end="fun")
+ 
+ #6
+d=int(input("Yoshingizni kiriting;"))
+print(f"Sizni yoshingiz:{d}")
+
+#7
+e='welkome'
+print("*,*7",{e},"*,*6")
+
+#8
+a=input("Biron so'z kiriting:")
+b=int(input("Biron harf kiriting:"))
+print(f"{a}\n{b}")
+
+#9
+a=20
+print(f"men {a} yoshman")
+
+#10
+a,b,c=A,B,C
+print(a,b,c, set="\n")
+
+#1
+my_number = 19
+print(my_number)
+
+#2
+my_demical = 3,14
+print(my_demical)
+
+#3
+is_true = True
+print(f"Javob: {is_true}")
+
+#4
+name = "Nematjon"
+age = 19
+print(f"Mening ismim: {name}, Yoshim:{age}")
+
+#5
+x = 1
+y = 2
+z = 3
+print(x,y,z, set=",")
+ 
+ #6
+a = 7
+b = a
+print(b)
+ 
+ #7
+x = "123"
+y = int(x)
+print(y)
+
+#8
+a = input("Son kiriting:")
+b = int(a)
+print(b)
+
+#9
+pi = 3,14159
+print(f"PI ni qiymati:{pi}")
+
+#10
+A = 10
+B = 20
+X = A
+B = A
+A = B
+print(A, B)
+
+#1
+num1 = 15
+num2 = 4
+a = num1+num2
+print(num1, num2)
+print(a)
+
+#2
+x=50
+y=23
+z=x-y
+print(z)
+
+#3
+x=5
+y=9
+z=x*y
+print(z)
+
+#4
+x=100
+y=10
+z=x/y
+print(z)
+
+#5
+a=17//3
+print(a)
+
+#6
+a=25%7
+print(a)
+
+#7
+a=(10+5)*2
+print(a)
+
+#8
+a=input("1chi sonni kiriting:")
+b=input("2chi sonni kiriting:")
+print(f"yig'indisi:{a + b}")
+print(f"ayirmasi:{a - b}")
+print(f"ko'paytmasi:{a * b}")
+print(f"bo'linmasi:{a / b}")
+
+#9
+a=input(f"Biron son kriting:")
+print(a*a)
+
+#10
+futbolka = "$25"
+shlyapa = "$15"
+umumiy = (int(futbolka)+int(shlyapa))
+print(umumiy)
+
+
